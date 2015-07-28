b61c1b587abff2bb53095172636b7da741f31aa4
Derrick Wong <derrickwong@Derricks-MacBook-Pro.local>
2015-07-28 11:55:28 +0800

    fixed for CF

--- springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
+++ springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
@@ -3,6 +3,7 @@ package io.pivotal.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * Microservice to manage user accounts.
  * 
